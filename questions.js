60a4f50375d3ba7c848e4bb6be06cf676a530020
oaspelin <oskar.aspelin@aalto.fi>
2015-04-21 13:09:45 +0300

    project meeting

---
+++ questions.js
@@ -0,0 +1,9 @@
+
+
+var Question1={Kysymys:"Kysymys", A:"X", B:"Y", C:"Z", D:"O", vaikeusaste:"Helppo" kuva: "images/bonneville1.jpg"};
+
+Question1.Kysymys
+
+var Questions=[Question1, Question2...];
+
+Question[1].Kysymys
