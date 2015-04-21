438f6cf21996cd987aa0b9a682fcc0e7d18d9389
oaspelin <oskar.aspelin@aalto.fi>
2015-04-21 09:50:58 +0300

    boxes

--- animation.js
+++ animation.js
@@ -1,3 +1,23 @@
 function loadGraphics() {
+	drawQuestionbox();
+	drawAnswerboxes();
+	drawPhase();
+}
 
+function drawQuestionbox(){
+	context.rect(60,60,600,160);
+	context.stroke();
+}
+
+function drawAnswerboxes(){
+	context.rect(60,240,290,100);
+	context.rect(370,240,290,100);
+	context.rect(60,360,290,100);
+	context.rect(370,360,290,100);
+	context.stroke();
+}
+
+function drawPhase(){
+	context.font = "bold 30px 'Electrolize'";
+	context.fillText("1/10",620,40);
 }
