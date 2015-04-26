60e68bc6609bb79d4a42a1377e71b79d18e80e4a
oaspelin <oskar.aspelin@aalto.fi>
2015-04-26 15:29:22 +0300

    kysymys vaihtuu hiirtä painamalla

--- game.js
+++ game.js
@@ -2,11 +2,39 @@ function runGame(backgroundImage1,questions){
 
 	//reacts on mouseclicks
 	listenToMouse();
+	var currentquestion;
+	var questionIndex=0;
+
 	requestAnimationFrame(gameLoop);
-	var currentquestion=questions[0];
+
+	function updateGameState(){
+		currentquestion=questions[questionIndex];
+	}
+
 	function gameLoop(time) {
+		updateGameState();
     	loadGraphics(time,backgroundImage1,currentquestion);
     	requestAnimationFrame(gameLoop);
-  }
+  	}
+
+  	function listenToMouse(){
+	document.addEventListener("mousedown",function(e){
+	    var pos=getMousePos(e);
+	    if((pos.X>60 && pos.X<300) && (pos.Y>240 && pos.Y<340)){
+			questionIndex++;
+		}
+	});
+
+	//converts the page coordinates to canvas coordinates
+	function getMousePos(event) {
+	    var rect = canvas.getBoundingClientRect();
+	    x = event.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+	    y = event.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+	    return {
+	        X: x - rect.left,
+	        Y: y - rect.top
+	    };
+	}
+}
 
 }
--- init.js
+++ init.js
@@ -23,20 +23,4 @@ function initCanvas() {
   runGame(images,questions);
 }
 
-function listenToMouse(){
-	document.addEventListener("mousedown",function(e){
-	    var pos=getMousePos(e);
-	    console.log(pos);
-	});
 
-	//converts the page coordinates to canvas coordinates
-	function getMousePos(event) {
-	    var rect = canvas.getBoundingClientRect();
-	    x = event.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
-	    y = event.clientY + document.body.scrollTop + document.documentElement.scrollTop;
-	    return {
-	        X: x - rect.left,
-	        Y: y - rect.top
-	    };
-	}
-}
