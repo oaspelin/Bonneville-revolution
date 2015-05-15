ed85bde412e730781b6e644057832d6107f54185
oaspelin <oskar.aspelin@aalto.fi>
2015-05-15 03:15:52 +0300

    loading screen kokeilua

--- init.js
+++ init.js
@@ -25,18 +25,48 @@ function initCanvas() {
 
   var contextIsSupported = context !== null;
 
+  //loading screen, because some of our files are quite large
+  context.fillStyle="black";
+  context.fillRect(0, 0, 720, 480);
+  var loading= setInterval(function(){
+    context.fillStyle="white"
+    context.font = "bold 70px 'Special Elite'";
+    context.textAlign="center";
+    context.fillText("Loading...", 360, 240);
+  }, 300);
+
+  var questions;
+  var images;
+  var highscores;
+  var audio;
 
   //loads the background Images
+  function initiateGame(){
+    questions=generateQuestions();
+    images=loadImages(questions);
+    audio = new Audio('sounds/Bike.wav');
+    highscores=loadHighscores();
+  }
   
-  var questions=generateQuestions();
-  var images=loadImages(questions);
-  var audio = new Audio('sounds/Bike.wav');
-  audio.play();
-  highscores=loadHighscores();
+  //clears the interval
+  clearInterval(loading);
 
   //starts the game
-  runGame(images,questions, highscores);
+  /*$.ajax({
+        'async': false,
+        'url': initiateGame(),
+        'success': function(){
+        runGame(images,questions, highscores);
+        audio.play();
+        console.log("hej");
+        }
+  })*/
 
+  $.when($.ajax(initiateGame())).then(function () {
+    console.log("hej");
+    runGame(images,questions, highscores);
+
+});
 }
 
 
